5d0be599bf45c6315bf4e24063b819efafc7de11
fiqryqq <fiqryqq@telkomuniversity.ac.id>
2021-04-10 16:49:33 +0700

    init

---
+++ app.js
@@ -0,0 +1,43 @@
+// Express init
+const express = require("express");
+const app = express();
+
+// local port
+const port = process.env.PORT || 5000;
+
+// Axios and cherrio
+const axios = require("axios");
+
+// baseUrl
+const url = "https://cekdiri.id/vaksinasi/";
+
+async function getData() {
+  try {
+    const response = await axios.get(url);
+
+    // Data isinya array data vaksinasi
+    const data = response.data.monitoring;
+
+    // Ambil data terakhir / hari ini
+    const latestData = data[data.length - 1];
+
+    return latestData;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+app.get("/", async (req, res) => {
+  res.send({
+    maintainer: "Fiqry choerudin <work.fiqrychoerudin@gmail.com>",
+    source: ""
+  });
+});
+
+app.get("/vaksin", async (req, res) => {
+  res.send(await getData());
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
